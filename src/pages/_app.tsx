2fb2350fd1cf976aea446ed39d1557b92ff3bd77
Pedro Lucas <pedrolucasfsouza@gmail.com>
2024-03-24 15:49:50 -0300

    Update: pixel de conversao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
+import ReactPixel from 'react-facebook-pixel';
 import {ChakraProvider} from '@chakra-ui/provider'
 import type {AppProps} from 'next/app'
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({Component, pageProps}: AppProps) {
+
+
+    ReactPixel.init('2134368583563440');
+    ReactPixel.pageView();
+    
     return (
         <ChakraProvider>
             <ToastContainer
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,15 @@
 import Analytics from "@/components/Analytics";
 import { Head, Html, Main, NextScript } from "next/document";
-import PixelFb from "./pixel-fb";
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <title>BOLETAAI - A Central de Inteligencia do Investidor</title>
-      <link rel="icon" href="/favicon.svg" sizes="any" />
+        <link rel="icon" href="/favicon.svg" sizes="any" />
       <Head />
       <body>
         <Main />
-        <NextScript />
-        <PixelFb />
+          <NextScript />
         <Analytics />
       </body>
     </Html>
