0e6ce5d3d29ee0d7520e231dff5aeba5d932df2a
Pedro Lucas <pedrolucasfsouza@gmail.com>
2024-03-24 16:18:22 -0300

    Pixel meta fb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,51 @@
 import '@/styles/globals.css'
-import ReactPixel from 'react-facebook-pixel';
 import {ChakraProvider} from '@chakra-ui/provider'
 import type {AppProps} from 'next/app'
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import * as fbq from '../lib/fpixel'
+import { useRouter } from 'next/router';
+import Script from "next/script";
+import { useEffect } from 'react';
 
 export default function App({Component, pageProps}: AppProps) {
+    const router = useRouter();
+
+  useEffect(() => {
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
 
-    ReactPixel.init('2134368583563440');
-    ReactPixel.pageView();
-    
     return (
         <ChakraProvider>
             <ToastContainer
             />
+            <Script
+                id="fb-pixel"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', ${fbq.FB_PIXEL_ID});
+                `,
+                }}
+             />
             <Component {...pageProps} />
         </ChakraProvider>
     )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,16 @@ export default function Document() {
     <Html lang="pt-BR">
       <title>BOLETAAI - A Central de Inteligencia do Investidor</title>
         <link rel="icon" href="/favicon.svg" sizes="any" />
-      <Head />
+      <Head> 
+      <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=2134368583563440&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </Head>
       <body>
         <Main />
           <NextScript />
